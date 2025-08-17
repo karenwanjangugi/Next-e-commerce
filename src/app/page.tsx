d45e0515300d5c314f1b3f461329d3fd20c71d92
karenwanjangugi <karenwanngugi@gmail.com>
2025-08-17 17:07:21 +0300

    added signup and login pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,9 @@
-import Footer from "./shared/footer";
-import Services from "./home/components/Services";
-import Featured from "./home/components/Featured";
-import { Products }from "./home/components/products";
-import Banner from "./home/components/banner";
-import { Browse } from "./home/components/browse";
-import Product from "./home/components/this-month";
-import NavBar from "./shared/navbar";
+import HomePage from "./home/page";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen mt-375">
-      <NavBar/>
-      <Browse/>
-      <Product/>
-      <Banner/>
-      <Products/>
-      <Featured/>
-      <Services/>
-      <Footer/>
+     <HomePage/>
     </div>
   );
 
