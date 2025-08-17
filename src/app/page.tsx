082073bce0a075f5012c40430dd73de8ab28665e
karenwanjangugi <karenwanngugi@gmail.com>
2025-08-17 17:09:33 +0300

    added signup and login pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import HomePage from "./home/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen mt-375">
+    <div className="mt-375">
      <HomePage/>
     </div>
   );
